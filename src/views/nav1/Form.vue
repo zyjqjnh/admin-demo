6c56fa478f5b5db90b2d0df9786f7f8c99eb2dd2
zhuyaojie <zyjqjnh@126.com>
2017-05-31 15:26:19 +0800

    form

--- src/views/nav1/Form.vue
+++ src/views/nav1/Form.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>form</h1>
+  <el-form ref="form" :model="form" label-width="80px" style="margin:20px;width:60%;min-width:600px;"></el-form>
 </template>
 
 <script>
-
+  export default {
+    data () {
+      return {
+        form: {
+          name: ''
+        }
+      }
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
